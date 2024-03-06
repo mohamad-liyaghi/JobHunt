e17be0e9c6ab4965bf02cbbd744f52d40ba48990
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2024-03-06 19:23:35 +0330

    feat(users): ping endpoint

---
+++ services/users/src/main.py
@@ -0,0 +1,19 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Users Service",
+    description="This is a simple users service.",
+    version="0.0.1",
+    redoc_url=None,
+    openapi_url="/api/v1/openapi.json",
+)
+
+
+@app.get("/ping")
+def ping():
+    return {"ping": "pong!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
