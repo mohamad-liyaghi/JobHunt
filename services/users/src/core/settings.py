3c9f10e04d77a90e96462465f3e2261e1c3081cd
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2024-03-11 14:55:24 +0330

    feat(users): JWT handler

--- services/users/src/core/settings.py
+++ services/users/src/core/settings.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class BaseConfig(BaseSettings):
-    model_config = SettingsConfigDict(env_file=("env/pg.env",))
+    model_config = SettingsConfigDict(env_file=("env/pg.env", "env/jwt.env"))
 
 
 class PgConfig(BaseConfig):
@@ -13,6 +13,14 @@ class PgConfig(BaseConfig):
     POSTGRES_PASSWORD: str
     POSTGRES_HOST: str
     POSTGRES_URL: str
+
+
+class JwtConfig(BaseConfig):
+    """This class is used to load environment variables for JWT from jwt.env"""
+
+    SECRET_KEY: str
+    JWT_ALGORITHM: str
+    JWT_EXPIRATION_MINUETS: int
 
 
 class Settings(PgConfig):
