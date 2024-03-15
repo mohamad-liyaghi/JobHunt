67418171ae176dcc6e9835ea6be14dff1c654391
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2024-03-15 14:35:44 +0330

    fix(posts): connection port

--- services/posts/main.go
+++ services/posts/main.go
@@ -12,18 +12,22 @@ var DB *gorm.DB
 
 func main() {
 	godotenv.Load()
+	println("Loading Environment Variables...")
 	postgresDB := os.Getenv("POSTGRES_DB")
 	postgresUser := os.Getenv("POSTGRES_USER")
 	postgresPassword := os.Getenv("POSTGRES_PASSWORD")
 	postgresHost := os.Getenv("POSTGRES_HOST")
 	postgresPort := os.Getenv("POSTGRES_PORT")
-	connection := "host=" + postgresHost + " port=" + postgresPort + " user=" + postgresUser + " dbname=" + postgresDB + " password=" + postgresPassword + " sslmode=disable"
+
+	println("Connecting to the database...")
+	connection := "host=" + postgresHost + " user=" + postgresUser + " password=" + postgresPassword + " dbname=" + postgresDB + " port=" + postgresPort
 	db, err := gorm.Open(postgres.Open(connection), &gorm.Config{})
 
 	if err != nil {
 		panic("Could not connect to the database...")
 	} else {
 		DB = db
+		println("Connected to the database...")
 	}
 
 	app := fiber.New()
