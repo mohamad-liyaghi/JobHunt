d39856b4d18d94d988308ae7323e388d2732816e
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2024-03-19 12:23:29 +0330

    feat(posts): list route

--- services/posts/controllers/posts.go
+++ services/posts/controllers/posts.go
@@ -1,9 +1,23 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	db "posts/config"
+	"posts/models"
+	"strconv"
+)
 
 func GetPosts(c *fiber.Ctx) error {
-	return c.SendString("Get Post Route!")
+	var posts []models.Post
+	limit, _ := strconv.Atoi(c.Query("limit", "10"))
+	offset, _ := strconv.Atoi(c.Query("offset", "0"))
+	var count int64
+
+	db.DB.Select("title", "created_at").Limit(limit).Offset(offset).Find(&posts).Count(&count).Order("created_at desc")
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"posts": posts,
+		"total": count,
+	})
 }
 
 func CreatePost(c *fiber.Ctx) error {
